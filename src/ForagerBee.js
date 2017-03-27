5d24cd271f8de3f334bfb4b5fe246a12424a3e57
Scott Moschella <scottmosch@gmail.com>
2017-03-27 12:26:23 -0700

    Implement ForagerBee subclass

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype.eat = Grub.prototype.eat;
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+};
